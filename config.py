c6d1bba70807079eec4a7343172e10323ed828f9
tcb <tcb@tencent.com>
2023-03-24 09:30:36 +0000

    init

--- config.py
+++ config.py
@@ -1,21 +1,9 @@
-# encoding: utf-8
+import os
 
-DIALECT = 'mysql'  # 要用的什么数据库
-DRIVER = 'pymysql'  # 连接数据库驱动
-USERNAME = 'root'  # 用户名
-PASSWORD = 'Root1234'  # 密码
-HOST = '127.0.0.1'  # 服务器
-PORT = '3306'  # 端口
-DATABASE = 'ideamerry_repair_v3'  # 数据库名
-DATABASE2 = 'bbq'  # 数据库名
+# 是否开启debug模式
+DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                                       DATABASE)
-
-SQLALCHEMY_BINDS = {
-    'bbq': "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
-                                                        DATABASE2)  # 另外配置的数据库
-}
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_POOL_RECYCLE = 8
+# 读取数据库环境变量
+username = os.environ.get("MYSQL_USERNAME", 'root')
+password = os.environ.get("MYSQL_PASSWORD", 'root')
+db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
